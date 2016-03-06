c63c69757c191630d540e4375e136f3912c5b3ef
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2016-03-06 21:40:44 +0800

    Add login form & localStorage to save the user's email and token

--- js/app.js
+++ js/app.js
@@ -14,4 +14,30 @@ app.controller('myCtrl', ['$scope', '$http', 'localStorageService', 'API_ENDPOIN
 	// LIST OF KEYS: 	console.log( localStorageService.keys() );
 	// DELETE LS: 		localStorageService.remove('test');
 
+	/*
+	Login form
+	Learn more: http://tutsnare.com/post-form-data-using-angularjs/
+	*/
+
+	// blank object to handle login form
+	$scope.user = {};
+
+	$scope.login = function(){
+		var formData = $scope.user; // Object {email: "user@example.com", password: "12345678"}
+
+    	$http.post('http://localhost:3000/users/sign_in.json', { user: formData }).
+    	then(
+			    function(response) {
+			        var response = response.data; // Object {status: "Success", message: "User signed-in successfully and token created", authentication_token: "9vCkNM-Qk8QkuxEDyXh6"}
+		        	if (response.status == "Success") {
+		        		console.log('Login success and token received');
+		        		localStorageService.set('user_email', $scope.user.email);
+		        		localStorageService.set('user_token', response.authentication_token);
+		        	}
+			    }, function(error) {
+			    	console.log('Login failed and no token received');
+			    }
+		    )
+    };
+
 }]);
