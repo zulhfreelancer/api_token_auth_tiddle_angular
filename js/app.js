30f7ed1011be0d2914718ebd7d3459e62f316186
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2016-03-06 18:00:50 +0800

    Add localStorage library + basic setup

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['LocalStorageModule']);
 
-app.controller('myCtrl', ['$scope', function ($scope) {
-	$scope.name = "Zulhilmi Zainudin";
+app.constant('API_ENDPOINT', "http://localhost:3000/")
+
+app.controller('myCtrl', ['$scope', '$http', 'localStorageService', 'API_ENDPOINT', function ($scope, $http, localStorageService, API_ENDPOINT) {
+	$scope.name = API_ENDPOINT;
+	
+	/*
+	For localStorage, we're using this library:
+	https://github.com/grevory/angular-local-storage
+	*/
+	// SET LS: 			localStorageService.set('test', 100)
+	// GET LS: 			console.log(localStorageService.get('test'));
+	// LIST OF KEYS: 	console.log( localStorageService.keys() );
+	// DELETE LS: 		localStorageService.remove('test');
+
 }]);
